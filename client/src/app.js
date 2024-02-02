4a2ecfced36cfa544f38eb1d8f50b2c6c6b39353
PSKutergin <PSKutergin@gmail.com>
2024-02-02 19:15:21 +0300

    create pages add and edit categories and types, and page balance

--- client/src/app.js
+++ client/src/app.js
@@ -3,6 +3,8 @@ import './assets/scss/form.scss';
 import './assets/scss/layout.scss';
 import './assets/scss/main.scss';
 import './assets/scss/categories.scss';
+import './assets/scss/addEdit.scss';
+import './assets/scss/balance.scss';
 
 import { Main } from './components/main';
 import pieChart from './components/charts';
