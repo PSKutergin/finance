bdf3e51aeb4ad48097ed8e97429979d1e0a3c838
PSKutergin <PSKutergin@gmail.com>
2024-03-04 01:02:52 +0300

    create calendar component

--- client/src/components/addEditOperations.js
+++ client/src/components/addEditOperations.js
@@ -1,4 +1,5 @@
 import { Operation } from '../services/operation';
+import Calendar from './calendar';
 
 export class AddEditOperations {
     constructor(mode) {
@@ -55,6 +56,7 @@ export class AddEditOperations {
         }
 
         if (this.inputDate && this.calendar && this.inputAmount) {
+            this.currentCalendar = new Calendar(this.calendar, this.inputDate);
             this.initInputs();
         }
 
@@ -81,7 +83,7 @@ export class AddEditOperations {
 
     initInputs() {
         this.inputDate.addEventListener('click', (e) => {
-            this.showCalendar(this.inputDate, this.calendar);
+            this.currentCalendar.showCalendar();
         })
 
         document.addEventListener('click', (e) => {
@@ -171,81 +173,5 @@ export class AddEditOperations {
         } catch (error) {
             console.log(error);
         }
-    }
-
-    showCalendar() {
-        let calendarHTML = '<div class="calendar-header">';
-        calendarHTML += '<button id="prevMonth">&#60;</button>';
-        calendarHTML += '<span>' + this.currentYear + ' - ' + this.currentMonth + '</span>';
-        calendarHTML += '<button id="nextMonth">&#62;</button>';
-        calendarHTML += '</div>';
-        calendarHTML += '<table>';
-        calendarHTML += '<thead><tr><th>Пн</th><th>Вт</th><th>Ср</th><th>Чт</th><th>Пт</th><th>Сб</th><th>Вс</th></tr></thead>';
-        calendarHTML += '<tbody>';
-
-        // Определяем первый день месяца
-        const firstDayOfMonth = new Date(this.currentYear, this.currentMonth - 1, 1).getDay();
-        // Определяем день недели, с которого начинается календарь (0 - Воскресенье, 1 - Понедельник, ..., 6 - Суббота)
-        const startingDayOfWeek = 1; // Начало недели с Понедельника (1)
-        let offset = firstDayOfMonth - startingDayOfWeek;
-        if (offset < 0) {
-            offset += 7;
-        }
-
-        // Добавляем пустые ячейки перед первым днем месяца
-        calendarHTML += '<tr>';
-        for (let i = 0; i < offset; i++) {
-            calendarHTML += '<td></td>';
-        }
-
-        // Добавляем дни месяца
-        const daysInMonth = new Date(this.currentYear, this.currentMonth, 0).getDate();
-        for (let day = 1; day <= daysInMonth; day++) {
-            if (offset % 7 === 0) {
-                calendarHTML += '</tr><tr>'; // Начинаем новую строку для следующей недели
-            }
-            calendarHTML += '<td>' + day + '</td>';
-            offset++;
-        }
-
-        calendarHTML += '</tr></tbody></table>';
-
-        this.calendar.innerHTML = calendarHTML;
-        this.calendar.classList.add('open');
-
-        this.calendar.addEventListener('click', (e) => {
-            e.preventDefault();
-            const target = e.target;
-            if (target.tagName === 'TD' && target.innerHTML !== '') {
-                const selectedDate = this.currentYear + '-' + this.currentMonth.toString().padStart(2, '0') + '-' + target.innerHTML.padStart(2, '0');
-                this.inputDate.value = selectedDate;
-                this.calendar.classList.remove('open');
-            }
-        });
-
-        this.initMonthSwitching();
-    }
-
-    initMonthSwitching() {
-        const prevMonthButton = document.getElementById('prevMonth');
-        const nextMonthButton = document.getElementById('nextMonth');
-        prevMonthButton.addEventListener('click', (e) => {
-            e.preventDefault();
-            this.currentMonth--;
-            if (this.currentMonth < 1) {
-                this.currentMonth = 12;
-                this.currentYear--;
-            }
-            this.showCalendar();
-        });
-        nextMonthButton.addEventListener('click', (e) => {
-            e.preventDefault();
-            this.currentMonth++;
-            if (this.currentMonth > 12) {
-                this.currentMonth = 1;
-                this.currentYear++;
-            }
-            this.showCalendar();
-        });
     }
 }
